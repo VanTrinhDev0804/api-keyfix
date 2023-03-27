2ba59d9f03f733abd3d9ae47d3e68ecbd5b33d8c
VanTrinhDev0804 <vantrinh2804@gmail.com>
2023-03-28 00:08:22 +0700

    push api

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require('body-parser')
-
-// const app = express();
-// require("dotenv").config();
-
-
-
-// app.use(cors());
-// app.use(express.json());
-// // Page Home
-// app.get("/", (req, res) => {
-//   res.send('SERVER ON')
-// })
-
-
-// app.get("*", (req, res) => {
-//   res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
-// });
-
-
-// app.listen(5000, () =>
-//   console.log(`Server started on`)
-// );
 
 const path = require('path')
 const express = require("express")
