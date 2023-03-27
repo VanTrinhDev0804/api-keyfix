eb35acaa84c60bf57159f1208e2ce3127807d79a
VanTrinhDev0804 <vantrinh2804@gmail.com>
2023-03-27 23:42:22 +0700

    fix pagehome1

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ app.use(bodyParser.json())
 
 app.use(cors());
 app.use(express.json());
-
-
-app.use("/api", authRoutes);
-
 // Page Home
 app.get("/", (req, res) => {
   res.send('SERVER ON')
 })
+app.use("/api", cors({ origin: '*' }), authRoutes);
+
+app.get("*", (req, res) => {
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+});
+
+
 app.listen(process.env.PORT, () =>
   console.log(`Server started on`)
 );
