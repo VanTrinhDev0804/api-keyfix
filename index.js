53248c8894ae01e9d99ea76f72f7ef60b4290b92
VanTrinhDev0804 <vantrinh2804@gmail.com>
2023-03-27 23:55:57 +0700

    fix pagehome3

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require('body-parser')
-const authRoutes = require("./routes/auth");
-const app = express();
-require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require('body-parser')
+// const authRoutes = require("./routes/auth");
+// const app = express();
+// require("dotenv").config();
 
 
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
+// // Page Home
+// app.get("/", (req, res) => {
+//   res.send('SERVER ON')
+// })
+// app.use("/api", authRoutes);
+
+// app.get("*", (req, res) => {
+//   res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+// });
+
+
+// app.listen(5000, () =>
+//   console.log(`Server started on`)
+// );
+
+const path = require('path')
+const express = require("express")
+require('dotenv').config()
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 3000
+
 // Page Home
 app.get("/", (req, res) => {
-  res.send('SERVER ON')
+    res.send('SERVER ON')
 })
-app.use("/api", authRoutes);
 
+// ZingMp3Router
+const ZingMp3Router = require("./src/routes/ZingRouter")
+app.use("/api", cors({ origin: '*' }), ZingMp3Router)
+
+// Page Error
 app.get("*", (req, res) => {
-  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+    res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
 });
 
-
-app.listen(5000, () =>
-  console.log(`Server started on`)
-);
+app.listen(port, () => {
+  console.log(`Start server listen at http://localhost:${port}`)
+});
